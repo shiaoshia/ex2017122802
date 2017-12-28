6872ef1ad6f57d839d36ffa765be42ccc89d522c
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2017-12-28 14:55:04 +0800

    隱性呼叫

--- app/src/main/java/tw/com/shiaoshia/ex2017122802/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2017122802/MainActivity.java
@@ -1,7 +1,9 @@
 package tw.com.shiaoshia.ex2017122802;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void click1(View v){
+        Intent it=new Intent();
+        it.setAction("DD123123");
+        startActivity(it);
+    }
+
 }
